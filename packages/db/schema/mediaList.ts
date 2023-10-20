3d5b103ffa988bc375808b1b73cd2c382ef91c9e
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-10-20 20:07:31 +0200

    mediaList schemas

---
+++ packages/db/schema/mediaList.ts
@@ -0,0 +1,39 @@
+import { index, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+
+import { baseColumns, dictionaryColumns } from "./commonColumns";
+
+export const mediaLists = mysqlTable(
+  "mediaLists",
+  {
+    mediaListTypeId: varchar("mediaListTypeId", { length: 255 }),
+
+    ...dictionaryColumns,
+    ...baseColumns,
+  },
+  (mediaList) => ({
+    idIdx: index("id_idx").on(mediaList.id),
+  }),
+);
+
+export const mediaListMedias = mysqlTable(
+  "mediaListMedias",
+  {
+    mediaId: varchar("mediaId", { length: 255 }),
+    mediaListId: varchar("mediaListId", { length: 255 }),
+
+    ...baseColumns,
+  },
+  (mediaListMedia) => ({
+    idIdx: index("id_idx").on(mediaListMedia.id),
+  }),
+);
+
+export const meidaListTypes = mysqlTable(
+  "meidaListTypes",
+  {
+    ...baseColumns,
+  },
+  (meidaListType) => ({
+    idIdx: index("id_idx").on(meidaListType.id),
+  }),
+);
