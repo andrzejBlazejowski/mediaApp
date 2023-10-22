c2b92bae5b7b2f19ac48a1d052ad55e670f0bc9a
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-10-22 14:25:24 +0200

    purchases relations

--- packages/db/schema/Purchase.ts
+++ packages/db/schema/Purchase.ts
@@ -1,6 +1,9 @@
+import { relations } from "drizzle-orm";
 import { index, mysqlTable, varchar } from "drizzle-orm/mysql-core";
 
+import { users } from "./auth";
 import { baseColumns, dictionaryColumns } from "./commonColumns";
+import { medias } from "./media";
 
 export const purchases = mysqlTable(
   "purchases",
@@ -16,6 +19,39 @@ export const purchases = mysqlTable(
   }),
 );
 
+export const purchasesRelations = relations(purchases, ({ many, one }) => ({
+  purchaseType: one(purchaseTypes, {
+    fields: [purchases.purchaseTypeId],
+    references: [purchaseTypes.id],
+  }),
+  user: one(users, { fields: [purchases.userId], references: [users.id] }),
+  purchaseItems: many(purchaseItems),
+}));
+
+export const purchaseItems = mysqlTable(
+  "purchaseItems",
+  {
+    purchaseId: varchar("purchaseId", { length: 255 }),
+    mediaId: varchar("mediaId", { length: 255 }),
+
+    ...baseColumns,
+  },
+  (purchaseItem) => ({
+    idIdx: index("id_idx").on(purchaseItem.id),
+  }),
+);
+
+export const purchaseItemsRelations = relations(purchaseItems, ({ one }) => ({
+  purchase: one(purchases, {
+    fields: [purchaseItems.purchaseId],
+    references: [purchases.id],
+  }),
+  media: one(medias, {
+    fields: [purchaseItems.mediaId],
+    references: [medias.id],
+  }),
+}));
+
 export const purchaseTypes = mysqlTable(
   "purchaseTypes",
   {
@@ -26,3 +62,7 @@ export const purchaseTypes = mysqlTable(
     idIdx: index("id_idx").on(purchaseType.id),
   }),
 );
+
+export const purchaseTypesRelations = relations(purchaseTypes, ({ many }) => ({
+  purchases: many(purchases),
+}));
