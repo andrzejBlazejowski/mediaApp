7fb50735b1c6332f732d95de8f0bdc4eb778d05e
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-10-22 14:32:10 +0200

    vod screen

--- packages/db/schema/vodScreen.ts
+++ packages/db/schema/vodScreen.ts
@@ -2,6 +2,7 @@ import { relations } from "drizzle-orm";
 import { index, mysqlTable, varchar } from "drizzle-orm/mysql-core";
 
 import { baseColumns, dictionaryColumns } from "./commonColumns";
+import { mediaLists } from "./mediaList";
 import { screens } from "./screen";
 
 export const vodScreens = mysqlTable(
@@ -36,6 +37,13 @@ export const vodScreenTypes = mysqlTable(
   }),
 );
 
+export const vodScreenTypesRelations = relations(
+  vodScreenTypes,
+  ({ many }) => ({
+    vodScreens: many(vodScreens),
+  }),
+);
+
 export const vodScreenMediaLists = mysqlTable(
   "vodScreenMediaLists",
   {
@@ -47,5 +55,19 @@ export const vodScreenMediaLists = mysqlTable(
   },
   (vodScreenMediaList) => ({
     idIdx: index("id_idx").on(vodScreenMediaList.id),
+  }),
+);
+
+export const vodScreenMediaListsRelations = relations(
+  vodScreenMediaLists,
+  ({ one }) => ({
+    vodScreen: one(vodScreens, {
+      fields: [vodScreenMediaLists.vodScreenId],
+      references: [vodScreens.id],
+    }),
+    mediaList: one(mediaLists, {
+      fields: [vodScreenMediaLists.mediaListId],
+      references: [mediaLists.id],
+    }),
   }),
 );
