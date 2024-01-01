45f79a0c3d12109795234f97e3435555e507a44f
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2024-01-01 17:39:36 +0100

    modified list

--- apps/backOffice/src/app/media/video-contents/page.tsx
+++ apps/backOffice/src/app/media/video-contents/page.tsx
@@ -5,56 +5,60 @@ import React, { useMemo } from "react";
 import { api } from "~/utils/api";
 import type { TableViewProps } from "../../_components/TableView";
 import { TableView } from "../../_components/TableView";
+import { title } from "./constants";
 
 export default function Page() {
-  const videoContents = api.videoContent.all.useQuery();
-  const deleteRow = api.videoContent.delete.useMutation();
   const utils = api.useUtils();
+
+  const rawData = api.videoContent.all.useQuery();
+  const deleteRow = api.videoContent.delete.useMutation();
+  const invalidate = utils.videoContent.all.invalidate;
+  const headersConfig = {
+    mediaId: {
+      orderNumber: 0,
+      name: "mediaId",
+      label: "mediaId",
+      classNames: "w-[100px]",
+      sortable: true,
+    },
+    videoId: {
+      orderNumber: 1,
+      name: "videoId",
+      label: "videoId",
+      classNames: "w-[100px]",
+      sortable: true,
+    },
+    type: {
+      orderNumber: 2,
+      name: "type",
+      label: "type",
+      classNames: "w-[100px]",
+      sortable: true,
+    },
+  };
 
   const mediaIndexProps = useMemo(() => {
     const data =
-      !videoContents.data || videoContents.data.length === 0
+      !rawData.data || rawData.data.length === 0
         ? []
-        : videoContents.data.map((videoContent) => {
+        : rawData.data.map((row) => {
             return {
-              mediaId: { value: videoContent.mediaId.toString() },
-              videoId: { value: videoContent.videoId.toString() },
-              type: { value: videoContent.videoContentTypeId.toString() },
-              id: { value: videoContent.id.toString() },
+              mediaId: { value: row.mediaId.toString() },
+              videoId: { value: row.videoId.toString() },
+              type: { value: row.videoContentTypeId.toString() },
+              id: { value: row.id.toString() },
             };
           });
     return {
-      title: "video content list",
+      title: title + " list",
       data: data,
-      headersConfig: {
-        mediaId: {
-          orderNumber: 0,
-          name: "mediaId",
-          label: "mediaId",
-          classNames: "w-[100px]",
-          sortable: true,
-        },
-        videoId: {
-          orderNumber: 1,
-          name: "videoId",
-          label: "videoId",
-          classNames: "w-[100px]",
-          sortable: true,
-        },
-        type: {
-          orderNumber: 2,
-          name: "type",
-          label: "type",
-          classNames: "w-[100px]",
-          sortable: true,
-        },
-      },
+      headersConfig,
       onDeleteRow: async (id) => {
         await deleteRow.mutateAsync(id);
-        await utils.videoContent.all.invalidate();
+        await invalidate();
       },
     } as TableViewProps;
-  }, [videoContents, deleteRow, utils]);
+  }, [rawData]);
 
   return <TableView {...mediaIndexProps}></TableView>;
 }
