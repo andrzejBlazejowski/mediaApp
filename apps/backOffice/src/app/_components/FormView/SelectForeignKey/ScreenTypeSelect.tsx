d9da2554a8e6dc407c619ac910c012e530e81410
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2024-01-06 13:37:50 +0100

    wip

---
+++ apps/backOffice/src/app/_components/FormView/SelectForeignKey/ScreenTypeSelect.tsx
@@ -0,0 +1,27 @@
+"use-client";
+
+import React, { useMemo } from "react";
+
+import { api } from "~/utils/api";
+import { SelectUi } from ".";
+import { SelectProps } from "./select.types";
+
+export default function ScreenTypeSelect(props: SelectProps) {
+  const rawData = api.screenType.all.useQuery();
+  const options = useMemo(
+    () =>
+      !rawData.data || rawData.data.length === 0
+        ? []
+        : rawData.data.map((row) => {
+            return {
+              value: row.id.toString(),
+              name: `${row.name} - ${row.id}`,
+            };
+          }),
+    [rawData],
+  );
+
+  return (
+    <SelectUi {...props} placeholder="select Screen type" options={options} />
+  );
+}
