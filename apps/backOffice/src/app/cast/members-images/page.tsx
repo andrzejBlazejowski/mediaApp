bb5b4257537796768b0dd9d26cb4113adb3ef067
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2024-01-25 17:11:56 +0100

    multiplication

--- apps/backOffice/src/app/cast/members-images/page.tsx
+++ apps/backOffice/src/app/cast/members-images/page.tsx
@@ -3,46 +3,68 @@
 import React, { useMemo } from "react";
 
 import type { TableViewProps } from "~/app/_components/TableView";
-import { TableView } from "~/app/_components/TableView";
+import { SortTypes, TableView } from "~/app/_components/TableView";
 import { api } from "~/utils/api";
 import { title } from "./constants";
+import { useHeadersConfig, useSort, useFilter } from "~/app/_lib/hooks";
 
 export default function Page() {
   const utils = api.useUtils();
+  const initialHeadersConfig = useMemo(
+    () => (  {
+      id: {
+        orderNumber: 0,
+        name: "id",
+        label: "id",
+        classNames: "w-[100px]",
+        
+  filterable: true,
+  sortDirection: SortTypes.None,
+      },
+      name: {
+        orderNumber: 1,
+        name: "name",
+        label: "name",
+        classNames: "w-[100px]",
+        
+  filterable: true,
+  sortDirection: SortTypes.None,
+      },
+      media: {
+        orderNumber: 1,
+        name: "media",
+        label: "media",
+        classNames: "w-[100px]",
+        
+  filterable: true,
+  sortDirection: SortTypes.None,
+      },
+      image: {
+        orderNumber: 2,
+        name: "image",
+        label: "image",
+        classNames: "w-[100px]",
+        
+  filterable: true,
+  sortDirection: SortTypes.None,
+      },
+    } ),
+    [],
+  );
 
-  const rawData = api.castMemberImage.all.useQuery();
+  const { headersConfig, setHeadersConfig } =
+    useHeadersConfig(initialHeadersConfig);
+  const { sort, onSortByColumn } = useSort(setHeadersConfig);
+  const { filter, onFilter, onFilterClear } = useFilter();
+
+
+
+  
+
+  const rawData = api.castMemberImage.all.useQuery(  { sort, filter });
   const deleteRow = api.castMemberImage.delete.useMutation();
   const invalidate = utils.castMemberImage.all.invalidate;
-  const headersConfig = {
-    id: {
-      orderNumber: 0,
-      name: "id",
-      label: "id",
-      classNames: "w-[100px]",
-      sortable: true,
-    },
-    name: {
-      orderNumber: 1,
-      name: "name",
-      label: "name",
-      classNames: "w-[100px]",
-      sortable: true,
-    },
-    media: {
-      orderNumber: 1,
-      name: "media",
-      label: "media",
-      classNames: "w-[100px]",
-      sortable: true,
-    },
-    image: {
-      orderNumber: 2,
-      name: "image",
-      label: "image",
-      classNames: "w-[100px]",
-      sortable: true,
-    },
-  };
+  const headersConfig =;
 
   const mediaIndexProps = useMemo(() => {
     const data =
@@ -60,6 +82,10 @@ export default function Page() {
       title: title + " list",
       data: data,
       headersConfig,
+      onSortByColumn,
+      onFilter,
+      onFilterClear,
+    
       onDeleteRow: async (id) => {
         await deleteRow.mutateAsync(id);
         await invalidate();
